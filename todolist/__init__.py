8b8a537cac95bd6af52e7de1f2de33abc0633b8f
Neha Malcom Francis <nehamalcom@gmail.com>
2021-07-17 13:39:20 +0530

    Added initialisation of db for test

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -26,5 +26,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello There!'
     
+    from . import db
+    db.init_app(app)
     
     return app
---
+++ todolist/schema.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+/*DROP TABLE IF EXISTS types_lists;
+DROP TABLE IF EXISTS list;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS type;
+DROP TABLE IF EXISTS today;*/
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+)
+/*
+CREATE TABLE type (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE item (
+    sl INTEGER,
+    title TEXT NOT NULL,
+    date_created DATE,
+    date_edited  DATE,
+    date_completed DATE,
+    description TEXT,
+    FOREIGN KEY ()
+);*/

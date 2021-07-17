42e55a3408f6dd8300f47b6a4b49b089583125d0
Neha Malcom Francis <nehamalcom@gmail.com>
2021-07-17 13:13:01 +0530

    Test application with only hello

---
+++ todolist/__init__.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # creating and configuring the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'todolist.sqlite'),
+    )
+
+    if test_config is None:
+        # if not testing, load the instance config
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensuring instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # testing example page
+    @app.route('/hello')
+    def hello():
+        return 'Hello There!'
+    
+    
+    return app

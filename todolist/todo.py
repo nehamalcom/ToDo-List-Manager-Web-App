8d0ab9223d73d4f94e2bd64283a33ee0aa6c393b
Neha Malcom Francis <nehamalcom@gmail.com>
2021-07-22 18:56:48 +0530

    Added item create, update and update templates and views

--- todolist/todo.py
+++ todolist/todo.py
@@ -34,8 +34,11 @@ def create():
             db.execute(
                 'INSERT INTO list (title, body, author_id) VALUES (?, ?, ?)', (title, body, g.user['id'])
             )
+            listid = db.execute(
+                'SELECT last_insert_rowid()'
+            ).fetchone()
             db.commit()
-            return redirect(url_for('todo.index'))
+            return redirect(url_for('todo.itemsindex', listid=int(listid[0])))
     return render_template('todo/create.html')
 
 def get_list(id, check_author=True):
@@ -81,3 +84,92 @@ def delete(id):
     db.execute('DELETE FROM list WHERE id = ?', (id, ))
     db.commit()
     return redirect(url_for('todo.index'))
+
+def get_item(listid, itemid, check_author=True):
+    list = get_db().execute(
+        'SELECT id, title, date_created, date_due, description, listid, completed FROM item WHERE listid = ? AND id = ?', (listid, itemid)
+    ).fetchone()
+    if list is None:
+        abort(404, f"Item id {itemid} doesn't exist.")
+    if check_author and list['author_id']!=g.user['id']:
+        abort(403)
+    
+    return list
+
+
+@bp.route('/<int:listid>')
+@login_required
+def itemsindex(listid):
+    db = get_db()
+    itemsoflist = db.execute(
+        'SELECT l.id, author_id, created, l.title, body, i.id, i.title, date_created, date_due, description, completed FROM list l JOIN item i ON listid = l.id WHERE listid = ? ORDER BY completed, date_due',(listid, )
+    ).fetchall()
+    return render_template('todo/itemsindex.html', items=itemsoflist, listid=listid)
+
+
+@bp.route('/<int:listid>/createitem', methods=('GET', 'POST'))
+@login_required
+def createitem(listid):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        date_due = request.form['date_due']
+        listid = listid
+        completed = 0
+        error = None
+        
+        if not title:
+            error = 'Title is required.'
+        if not date_due:
+            error = 'Date due is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO item (title, date_due, description, listid, completed) VALUES (?, ?, ?, ?, ?)', (title, date_due, description, listid, completed)
+            )
+            db.commit()
+            return redirect(url_for('todo.itemsindex', listid=listid))
+    return render_template('todo/createitem.html')
+
+
+@bp.route('/<int:listid>/<int:itemid>/update', methods=('GET', 'POST'))
+@login_required
+def updateitem(listid, itemid):
+    list = get_list(listid)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        date_due = request.form['date_due']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+        if not date_due:
+            error = 'Date due is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE item SET title = ?, description = ? WHERE listid = ? AND itemid = ?', (title, description, listid, itemid)
+            )
+            db.commit()
+            return redirect(url_for('todo.itemsindex', listid = listid))
+    return render_template('todo/updateitem.html', list=list)
+
+
+
+@bp.route('/<int:listid>/<int:itemid>/delete', methods=('POST', ))
+@login_required
+def deleteitem(listid, itemid):
+    get_list(listid)
+    get_item(itemid)
+    db = get_db()
+    db.execute('DELETE FROM item WHERE listid = ? AND itemid = ?', (listid, itemid))
+    db.commit()
+    return redirect(url_for('todo.itemsindex', listid=listid))

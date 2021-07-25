8573b283a53043ba19d8a3668648bf5650dd3370
Neha Malcom Francis <nehamalcom@gmail.com>
2021-07-26 01:13:38 +0530

    Added login and sign-up page frontend using React

---
+++ todo-react/src/components/login.component.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign In</h3>
+
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" className="form-control" placeholder="Enter username" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+
+            </form>
+        );
+    }
+}
